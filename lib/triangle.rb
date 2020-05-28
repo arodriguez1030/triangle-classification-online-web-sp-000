2ef1dc82afda1faebfa4c4b49a720ff49533304c
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-28 00:31:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     if @left + @right <= @middle || @middle = @left <= @right || @right + @middle <= @left || @left * @middle * @right == 0
       raise TriangleError
     elsif @left == @middle && @middle == @right
-    
+    self.kind == :equilateral
         end
   end
   
