ce9e29de98f9fb5269691750462003789847ca67
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-28 00:40:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Triangle
   attr_accessor :left, :right, :middle, 
-  attr_writer
+  attr_writer :kind
   def initialize (left, right, middle)
     @left = left
     @right = right
