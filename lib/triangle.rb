7a6fab12608a1077c6bd74cb7337b9e9695f372d
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-05-28 00:30:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,8 @@ class Triangle
   def kind
     if @left + @right <= @middle || @middle = @left <= @right || @right + @middle <= @left || @left * @middle * @right == 0
       raise TriangleError
+    elsif @left == @middle && @middle == @right
+    
         end
   end
   
